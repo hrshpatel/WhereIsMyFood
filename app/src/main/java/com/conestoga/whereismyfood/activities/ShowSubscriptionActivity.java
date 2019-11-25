95965a9d135f34e1f12718ba4a4b05052e46535d
Harsh Patel <hrshpatel06@gmail.com>
2019-11-25 14:07:35 -0500

    Created activity to add subscription

--- app/src/main/java/com/conestoga/whereismyfood/activities/ShowSubscriptionActivity.java
+++ app/src/main/java/com/conestoga/whereismyfood/activities/ShowSubscriptionActivity.java
@@ -1,16 +1,21 @@
 package com.conestoga.whereismyfood.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.conestoga.whereismyfood.R;
+import com.conestoga.whereismyfood.databinding.ActivityShowSubscriptionBinding;
 
 public class ShowSubscriptionActivity extends AppCompatActivity {
+
+    ActivityShowSubscriptionBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_subscription);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_show_subscription);
     }
 }
